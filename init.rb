9d519226246ae61c3bc9baa96aeaddc0d2f3f3f5
glorydays <glorydays11@gmail.com>
2026-02-23 23:10:53 +0900

    Update init.rb

--- init.rb
+++ init.rb
@@ -11,11 +11,11 @@ end
 
 Redmine::Plugin.register :redmine_report do
   name 'Redmine Report plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'tiohsa'
+  description 'Report plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/tiohsa/redmine_report'
+  author_url 'https://github.com/tiohsa/redmine_report'
 
   project_module :schedule_report do
     permission :view_schedule_report,
